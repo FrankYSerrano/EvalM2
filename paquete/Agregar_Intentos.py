68e930be5307a382327531ab5bf4a34cc2b1347f
FrankYSerrano <FRANKYSERRANO@GMAIL.COM>
2020-11-10 19:21:32 -0300

    Funcion que agrega el input a la lista de intentos agregandole el .

--- paquete/Agregar_Intentos.py
+++ paquete/Agregar_Intentos.py
@@ -1,6 +1,24 @@
-def g_1(a, b):
-    c=a+b
-    return c
+def agrega_intentos(param_intentos, param_valor):
+    #Variable para almacenar la conversion de lista a String
+    cadena_caracteres =""
+
+    #ciclo que recorre la lista param_valor la cual siempre tiene 6 elementos
+    for i in range(0, 6):
+        #Concatenado de los elementos de la lista en un string
+        cadena_caracteres = cadena_caracteres + str(param_valor[i])
+    #Si los 1eros 3 digitos del string son mayores a cero entonces el numero es mayor a mil y requiere insertarle el .
+    if int(cadena_caracteres[0:3]) > 0:
+        cadena_caracteres = str(int(cadena_caracteres[0:3])) + "." + cadena_caracteres[3:6]
+    else:
+        #Si es menor a mil el string debe cambiar eliminandole los ceros de la parte delantera
+        cadena_caracteres = str(int(cadena_caracteres[3:6]))
+    param_intentos.append(cadena_caracteres)
+#    print(param_intentos)
 
 if __name__ == "__main__":
-    print(str(g_1(2,3)))
+    agrega_intentos(['4.000'], [0,0,0,0,0,0])
+    agrega_intentos(['4.000'], [0,0,0,0,2,0])
+    agrega_intentos(['4.000'], [0,0,0,3,0,0])
+    agrega_intentos(['4.000'], [0,0,4,0,0,0])
+    agrega_intentos(['4.000'], [0,5,0,0,0,0])
+    agrega_intentos(['4.000'], [6,0,0,0,0,0])
