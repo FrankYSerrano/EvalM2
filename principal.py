9a142e05f57a0026800af9566b1d8e93c640ffbc
FrankYSerrano <FRANKYSERRANO@GMAIL.COM>
2020-11-10 16:32:58 -0300

    Inicio de Evaluacion

--- principal.py
+++ principal.py
@@ -1,7 +1,18 @@
 #script principal
 import paquete as p
 
-print(str(p.c1(4,2)))
-print(str(p.fe_1(5,7)))
-print(str(p.fr_1(8,8)))
-print(str(p.g_1(9,9)))
+intento = False
+
+while not intento:
+    #Ingresa_Dato
+        intento=True
+    #Descomponer_En_Diccionario
+    #Mostrar_Abaco
+    #Agregar_Intentos
+
+    print(str(p.c1(4,2)))
+    print(str(p.fe_1(5,7)))
+    print(str(p.fr_1(8,8)))
+    print(str(p.g_1(9,9)))
+
+print("FIN")
