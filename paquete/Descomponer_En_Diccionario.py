2a4e594a8e3d3d57b05908efeb53969f2a9ea4b5
FrankYSerrano <FRANKYSERRANO@GMAIL.COM>
2020-11-11 13:55:24 -0300

    Ajustes a programa principal y algunas funciones

--- paquete/Descomponer_En_Diccionario.py
+++ paquete/Descomponer_En_Diccionario.py
@@ -3,8 +3,9 @@ def agrega_diccionario(param_valor,diccionario):
     diccionario ["decena"] = param_valor[4]
     diccionario ["centena"] = param_valor[3]
     diccionario ["unidad_mil"] = param_valor[2]
-    diccionario ["decena_mil"] = param_valor[1]0]
-    print(diccionario)
+    diccionario ["decena_mil"] = param_valor[1]
+    diccionario ["centena_mil"] = param_valor[0]
+#    print(diccionario)
 
 if __name__ == "__main__":
     agrega_diccionario([6, 5, 4, 3, 2, 1], {"centena_mil":0, "decena_mil":0, "unidad_mil":0, "centena":0, "decena":0, "unidad":0})
