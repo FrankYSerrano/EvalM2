5a61cf99633581363163c57b6aaad137f465e941
FrankYSerrano <FRANKYSERRANO@GMAIL.COM>
2020-11-10 23:03:33 -0300

    Ajustes a programa principal

--- principal.py
+++ principal.py
@@ -4,9 +4,9 @@ import paquete as p
 #INICIALIZACIONE DE VARIABLES
 
 #VARIABLE QUE PERMITIRA CONTROLAR EL LOOP
-intento = False
+intento = True
 #VARIABLE DE TIPO LISTA QUE ALMACENARA EL VALOR DE CADA POSICION
-valor = [0, 0, 0, 0, 0, 0]
+valorL = [0, 0, 0, 0, 0, 0]
 
 #VARIABLE QUE ALMACENARA LOS INTENTOS INGRESADOS POR EL USUARIO
 intentos = []
@@ -14,14 +14,25 @@ intentos = []
 #VARIABLE QUE SERVIRA PARA ALMACENAR LA CANTIDAD DE CADA UNIDAD DE DESCOMPOSICION
 diccionario_unidades = {"centena_mil" : 0, "decena_mil" : 0, "unidad_mil" : 0, "centena" : 0, "decena" : 0, "unidad" : 0}
 
-while not intento:
-    Ingresa_Dato(intento, valor)   CHRISTIAN
-    if not intento:
-        break
-    else:
+otro = ''
+
+while intento:
+    p.preguntar_numero(not intento, valorL)
+#    print(intento)
+#    print(valorL)
+    p.agrega_intentos(intentos, valorL)
+#    print(intentos)
+#    print(valorL)
+#    if not intento:
+#        break
+#    else:
         #Descomponer_En_Diccionario(valor, diccionario_unidades)    FERNANDO
         #Mostrar_Abaco(diccionario_unidades)    GUIDO
-        #Agregar_Intentos(intentos, valor)      FRANK
+    print("abaco y diccionario")
+
+    otro = input("Desea ingresar otro valor? S/N ")
+    if otro.upper() == 'N':
+        intento = False
 
 print("Los ingresos fueron: " + str(intentos))
 print("FIN")
