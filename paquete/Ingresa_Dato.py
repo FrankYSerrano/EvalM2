7e1bcb406ea8d2aae6738c33002a568b92288f2d
cfigueroa50 <cristian.figueroa@asesorti.com>
2020-11-11 15:25:35 -0300

    actulizar ya hice comentarios

--- paquete/Ingresa_Dato.py
+++ paquete/Ingresa_Dato.py
@@ -1,14 +1,17 @@
-def preguntar_numero(param_intento, param_valor):
-    while not param_intento:
-        valor = input("Ingrese un número entero: ")
-        if valor.upper() == "SALIR":
-            break 
+ #funcion que valida el dato ingresado en el menu principal:validacion de ingreso o sale del programa, 
+ # velidacion de numero que no se ingrese palabras y validacion de numero entero.
+
+def preguntar_numero(param_intento, param_valor): 
+       while not param_intento: # loop que valida si sigue o sale del programa
+        valor = input("Ingrese un número entero: ")  
+        if valor.upper() == "SALIR":                 #valida que lo ingresado es numero y no letra
+            break                                   
         else:   
             try:
-                valorentero=int(valor)
+                valorentero=int(valor)                #validador que lo ingresado es un numero entero
                 if valorentero > 999999:
-                    print(" el numero se pasa de los parametros")
-                else:  
+                    print(" el numero se pasa de los parametros") # restringido para que se ingresen cifras menores que 
+                else:                                             # 999.999
                     if valorentero < 99999:
                         param_valor[0]=0
                     if valorentero < 9999:
@@ -18,21 +21,13 @@ def preguntar_numero(param_intento, param_valor):
                     if valorentero < 99:
                         param_valor[3]=0    
                     if valorentero < 9:
-                        param_valor[4]=0 
-                    
-                    for i in range(5, 5-len(valor), -1):
-                    # print(str(i))
-                    # print(str( i-5 + len(valor)))
-                        param_valor[i] = int(valor[i-6 + len(valor)])
-                # print(param_valor)            
-                        #  for i in range(0, len(valor)):
-                    #  print(valor[i])
-                    #  param_valor[i]=int(valor[i])
-                    #param_intento=True
-                # print(str(len(valor)))
-            except ValueError:
+                        param_valor[4]=0    
+
+                    for i in range(5, 5-len(valor), -1):               # funcion que sirve para recorrer la lista y           
+                        param_valor[i] = int(valor[i-6 + len(valor)])  # dejarala en orden ademas de colocar cero en el inicio de la cifra 
+            except ValueError:                                         # para lograr las cantidades de fichas              
                 print("ATENCIÓN: Debe ingresar un cifra de valor  entero.")
   #  print(param_valor)        
- 
 if __name__ == "__main__":
     preguntar_numero(False,[0, 0, 0, 0, 0, 0])
+    
